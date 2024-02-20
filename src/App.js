44df26ace3db561486186838c96f7f31de4f230e
DavidNavarroSaiz <dcnavarros@unal.edu.co>
2024-02-20 17:37:57 -0500

    DnD working

--- src/App.js
+++ src/App.js
@@ -20,20 +20,27 @@ const initialTasks = [
   },
 
 ]
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+}
+
 function App() {
   const [tasks, setTasks] = useState(initialTasks);
   return (
-    <DragDropContext onDragEnd={(result) => 
-    {
-      const {source,destination}=result;
-      if (!destination){
+    <DragDropContext onDragEnd={(result) => {
+      const { source, destination } = result;
+      if (!destination) {
         return;
       }
-      if (source.index ===destination.index &&
-        source.droppableId === destination.droppableId)
-    {
+      if (source.index === destination.index &&
+        source.droppableId === destination.droppableId) {
         return;
       }
+      setTasks(prevTasks => reorder(prevTasks, source.index, destination.index))
 
     }}>
       <div className="app">
@@ -44,14 +51,14 @@ function App() {
               {...droppableProvided.droppableProps}
               ref={droppableProvided.innerRef}
               className="task-container">
-              {tasks.map((task,index) => (
-                <Draggable key={task.id} draggableId={task.id} index = {index}>
-                {(draggableProvided)=> <li 
-                {...draggableProvided.draggableProps}
-                ref  = {draggableProvided.innerRef}
-                {...draggableProvided.dragHandleProps}
-                 className='task-item'
-                 > {task.text}</li>}
+              {tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(draggableProvided) => <li
+                    {...draggableProvided.draggableProps}
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.dragHandleProps}
+                    className='task-item'
+                  > {task.text}</li>}
                 </Draggable>
               ))}
               {droppableProvided.placeholder}
